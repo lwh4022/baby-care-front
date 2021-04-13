bf445baccee673f2ed86c5f6a60ece5c6fb47dc3
LeeWonHee <lwh4022>
2021-04-13 23:14:09 +0900

    final

--- __test__/test.ts
+++
@@ -1,5 +0,0 @@
-import utils from '../server/utils/utilFunc'
-
-test('test', () => {
-  expect(utils.authorize()).toEqual('token');
-})
--- jest.config.babel.js
+++
@@ -1,7 +0,0 @@
-const path = require('path');
-module.exports = {
-  transform: {
-    '^.+\\.[js|jsx|ts|tsx]?$': path.resolve(__dirname, './jest.config.babel.js'),
-  },
-  testRegex: '/__tests__/*'
-};
